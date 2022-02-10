20a8c58c02bc62560bfdd525e9c89dc3c12da09a
Devanir <junior472399@gmail.com>
2022-02-10 17:14:32 -0300

    [View] Ajustes na pagina das cameras

--- frontend/src/views/Cameras.vue
+++ frontend/src/views/Cameras.vue
@@ -1,76 +1,63 @@
 <template>
-    <div v-bind:style="{backgroundColor: color}">
+    <div style="height: 100%" v-bind:style="{backgroundColor: color}">
         <harpiaBar/>
-        dev ainda não está na área<br>
-        Área de texto acima das cameras
-
-        <div class="tabs is-fullwidth is-toggle">
+        <div class="tabs is-centered is-toggle mt-2">
           <ul>
-            <li class="is-active" @click="tabSwitcher('1', $event)">
-              <a>
+            <li class="is-active" @click="tabSwitcher('1')">
+              <a id="tabs" class="mr-2">
                 <span>Área A</span>
               </a>
             </li>
-            <li @click="tabSwitcher('2', $event)">
-              <a>
+            <li @click="tabSwitcher('2')">
+              <a id="tabs" class="mr-2">
                 <span>Área B</span>
               </a>
             </li>
-            <li @click="tabSwitcher('3', $event)">
-              <a>
+            <li @click="tabSwitcher('3')">
+              <a id="tabs">
                 <span>Área C</span>
               </a>
             </li>
           </ul>
         </div>
 
-        <div id="tab-content">
-            <div id="camsA" v-if="group=='1'">
-                <div id="camsA1" class="columns is-multiline">
-                    <div class="column is-half">
-                        <img src="@/assets/cam1.jpg" alt="camera 1"/>
-                    </div>
-                    <div class="column is-half">
-                        <img src="@/assets/cam2.jpg" alt="camera 2"/>
-                    </div>
+        <div id="tab-content" class=" ml-3 mr-3">
+            <div id="camsA" v-if="group=='1'" class="columns is-multiline is-gapless">
+                <div class="column is-half">
+                    <img src="@/assets/cam1.jpg" alt="camera 1"/>
                 </div>
-
-                <div id="camsA2" class="columns is-multiline">
-                    <div class="column is-half">
-                        <img src="@/assets/cam3.jpg" alt="camera 3"/>
-                    </div>
-                    <div class="column is-half">
-                        <img src="@/assets/cam4.jpg" alt="camera 4"/>
-                    </div>
+                <div class="column is-half">
+                    <img src="@/assets/cam2.jpg" alt="camera 2"/>
+                </div>
+                <div class="column is-half ">
+                    <img src="@/assets/cam3.jpg" alt="camera 3"/>
+                </div>
+                <div class="column is-half">
+                    <img src="@/assets/cam4.jpg" alt="camera 4"/>
                 </div>
             </div>
 
-            <div id="camsB" class="columns is-multiline" v-if="group=='2'">
-                <div class="column is-half">
-                    <img src="@/assets/cam5.jpg" alt="camera 3"/>
+            <div id="camsB" v-if="group=='2'" class="columns is-multiline is-gapless">
+                <div class="column is-full">
+                    <img src="@/assets/cam5.jpg" alt="camera 5"/>
                 </div>
-                <div class="column is-half">
-                    <img src="@/assets/cam6.jpg" alt="camera 4"/>
+                <div class="column is-full">
+                    <img src="@/assets/cam6.jpg" alt="camera 6"/>
                 </div>
             </div>
 
-            <div id="camsC" v-if="group=='3'">
-                <div id="camsC1" class="columns is-multiline">
-                    <div class="column is-half">
-                        <img src="@/assets/logo.png" alt="camera 1"/>
-                    </div>
-                    <div class="column is-half">
-                        <img src="@/assets/cam2.jpg" alt="camera 2"/>
-                    </div>
+            <div id="camsC" v-if="group=='3'" class="columns is-multiline is-gapless">
+                <div class="column is-half">
+                    <img src="@/assets/patio01.jpg" alt="camera 7"/>
                 </div>
-
-                <div id="camsC2" class="columns is-multiline">
-                    <div class="column is-half">
-                        <img src="@/assets/cam3.jpg" alt="camera 3"/>
-                    </div>
-                    <div class="column is-half">
-                        <img src="@/assets/sound_logo.png" alt="camera 4"/>
-                    </div>
+                <div class="column is-half">
+                    <img src="@/assets/patio02.jpg" alt="camera 8"/>
+                </div>
+                <div class="column is-half">
+                    <img src="@/assets/cam5.jpg" alt="camera 9"/>
+                </div>
+                <div class="column is-half">
+                    <img src="@/assets/cam6.jpg" alt="camera 10"/>
                 </div>
             </div>
         </div>
@@ -88,12 +75,12 @@ export default {
     },
     data() {
         return {
-            color: '#16161c',
+            color: 'hsl(200, 2%, 2%)',
             group: '1',
         }
     },
     methods: {
-        tabSwitcher(g, event) {
+        tabSwitcher(g) {
             this.group = g
             const tabs = document.querySelectorAll('.tabs li')
             tabs.forEach((tab) => {
@@ -105,3 +92,16 @@ export default {
 }
 
 </script>
+
+<style>
+#tabs {
+    width: 12vw;
+    background: hsl(205, 90%, 50%);
+    border-radius: 24px;
+}
+
+.is-active {
+    transform: scale(0.83);
+}
+
+</style>
